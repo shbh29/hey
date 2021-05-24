4448cb5198d27bcd2721b8dfbd2d2516552538ea
Shubham Singh <ssingh@shubham.local>
2021-05-24 11:41:05 +0530

    used external library.

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/shbh29/hey
+
+go 1.16
+
+require (
+	github.com/shbh29/gopkg v0.0.0-20210524055800-35bd0c7c1ed0
+	rsc.io/quote v1.5.2
+)
---
+++ hey.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	"rsc.io/quote"
+	"github.com/shbh29/gopkg"
+)
+
+func main() {
+	fmt.Println(quote.Hello())
+	fmt.Println(gopkg.Hello())
+}
